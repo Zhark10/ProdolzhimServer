e6d377b55e4fb1e21b4f3ccf6fa165830939d8fc
Zhark10 <arkady.zharavin@citronium.biz>
2020-01-31 20:33:05 +0300

    added methods in token service

--- src/token/token.service.ts
+++ src/token/token.service.ts
@@ -1,4 +1,28 @@
+import { CreateUserTokenDto } from './dto/create-user-token.dto';
+import { IUserToken } from './interfaces/user-token-interfaces';
+import { ModelName } from './../global/models';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class TokenService {}
+export class TokenService {
+    constructor(@InjectModel(ModelName.Token) private readonly tokenModel: Model<IUserToken>) { }
+
+    async create(createUserTokenDto: CreateUserTokenDto): Promise<IUserToken> {
+        const userToken = new this.tokenModel(createUserTokenDto);
+        return await userToken.save();
+    }
+
+    async delete(uid: string, token: string): Promise<{ ok?: number, n?: number }> {
+        return await this.tokenModel.deleteOne({ uid, token });
+    }
+
+    async deleteAll(uid: string): Promise<{ ok?: number, n?: number }> {
+        return await this.tokenModel.deleteMany({ uid });
+    }
+
+    async exists(uid: string, token: string): Promise<boolean> {
+        return await this.tokenModel.exists({ uid, token });
+    }
+}
